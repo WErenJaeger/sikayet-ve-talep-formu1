5fd0aa6115fbce0f0b480ac2bdd353b10feab20f
WErenJaeger <161383146+WErenJaeger@users.noreply.github.com>
2024-08-15 14:33:45 +0300

    Create sayfakapat.php

---
+++ sayfakapat.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+
+header("refresh:40;url=logout.php");
+
+
+if (!isset($_SESSION['logged_in'])) {
+    header('Location: login.php');
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sayfa Zaman Aşımı</title>
+</head>
+<body>
+    <h1>Bu sayfa 70 saniye sonra kapanacak</h1>
+</body>
+</html>
